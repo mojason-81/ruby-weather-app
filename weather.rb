be06dc9cd2b9878fe33259ed52405b33d58fffef
mojason-81 <jason@mojason.com>
2015-03-22 21:43:50 -0500

    modified weather.rb

--- weather.rb
+++ weather.rb
@@ -1,3 +1,5 @@
+#require "open-uri"
+#require "nokogiri"
 require "yahoo_weatherman"
 
 puts "Enter your location via your zip code"
@@ -10,11 +12,17 @@ weather = weather * 9 / 5 + 32
 puts "Your current temperature is #{weather} degrees Farenheit with #{condition} conditions."
 puts
 new_weather = client.lookup_by_location(zip)
-new_weather.methods.each{
-	|meth| puts meth
-}
+#new_weather.methods.each{
+#	|meth| puts meth
+#}
 puts
 forecast = client.lookup_by_location(zip).forecasts
-forecast.each{
-	|item| puts item
+today = Time.now.strftime("%w").to_i
+tomorrow = Time.now.strftime("%w").to_i + 1
+forecast.each{ |item|
+	low = item["low"]
+	high = item["high"]
+	day = item["day"]
+	text = item["text"]
+	puts item["date"]
 }
