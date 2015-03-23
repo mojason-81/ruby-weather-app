060fb6f0973449ecd1150d4553ea64e3913b0388
mojason-81 <jason@mojason.com>
2015-03-22 20:02:30 -0500

    modified weather.rb

--- weather.rb
+++ weather.rb
@@ -8,3 +8,13 @@ condition = client.lookup_by_location(zip).condition['text']
 weather = weather.to_i
 weather = weather * 9 / 5 + 32
 puts "Your current temperature is #{weather} degrees Farenheit with #{condition} conditions."
+puts
+new_weather = client.lookup_by_location(zip)
+new_weather.methods.each{
+	|meth| puts meth
+}
+puts
+forecast = client.lookup_by_location(zip).forecasts
+forecast.each{
+	|item| puts item
+}
