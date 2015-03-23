bddbf014d46cbd19e4610a98cdecdb886c8aabe7
mojason-81 <jason@mojason.com>
2015-03-23 13:24:15 -0500

    modified weather.rb

--- weather.rb
+++ weather.rb
@@ -1,7 +1,5 @@
-#require "open-uri"
-#require "nokogiri"
 require "yahoo_weatherman"
-
+puts
 puts "Enter your location via your zip code"
 zip = gets.chomp
 client = Weatherman::Client.new
@@ -9,20 +7,29 @@ weather = client.lookup_by_location(zip).condition['temp']
 condition = client.lookup_by_location(zip).condition['text']
 weather = weather.to_i
 weather = weather * 9 / 5 + 32
-puts "Your current temperature is #{weather} degrees Farenheit with #{condition} conditions."
 puts
+puts "Your current temperature is #{weather} degrees Farenheit with #{condition} conditions."
 new_weather = client.lookup_by_location(zip)
-#new_weather.methods.each{
-#	|meth| puts meth
-#}
 puts
 forecast = client.lookup_by_location(zip).forecasts
 today = Time.now.strftime("%w").to_i
 tomorrow = Time.now.strftime("%w").to_i + 1
+
 forecast.each{ |item|
-	low = item["low"]
-	high = item["high"]
+	low = item["low"] * 9 / 5 + 32
+	high = item["high"] * 9 / 5 + 32
 	day = item["day"]
+	weekday = item["date"]
+	day_of_week = weekday.strftime("%w").to_i
+	#puts day_of_week
 	text = item["text"]
-	puts item["date"]
+	#puts item["date"]
+	
+	if day_of_week == today
+		day = "Today"
+	elsif day_of_week == tomorrow
+		day = "Tomorrow"
+	end
+	puts "#{day} will be #{text}.  The low temp will be #{low} and the high will be #{high}."
+	puts
 }
